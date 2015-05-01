873e317db2ce8a87758084b53aed07841e3deeab
Brian Tomlinson <darthlukan@gmail.com>
2015-05-01 11:47:18 +0200

    fizzbuzz

---
+++ fizzbuzz/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace fizzbuzz
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int[] nums = new int[10] { 100, 22, 13, 15, 24, 32, 30, 40, 90, 55 };
+
+			foreach (int num in nums) {
+				if (num % 3 == 0 && num % 5 == 0) {
+					Console.WriteLine (num + ": FizzBuzz!");
+				} else if (num % 3 == 0) {
+					Console.WriteLine (num + ": Fizz!");
+				} else if (num % 5 == 0) {
+					Console.WriteLine (num + ": Buzz!");
+				} else {
+					Console.WriteLine (num);
+				}
+			}
+		}
+	}
+}
