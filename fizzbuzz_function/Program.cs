8dea775a7b86d2cb2155337b7bc56f2659629e8f
Brian Tomlinson <darthlukan@gmail.com>
2015-05-01 13:09:30 +0200

    fizzbuzz with a function

---
+++ fizzbuzz_function/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace fizzbuzz_function
+{
+	class MainClass
+	{
+		public static string FizzBuzzMessage (int num)
+		{
+			string msg;
+
+			if (num % 3 == 0 && num % 5 == 0) {
+				msg = num + ": FizzBuzz!";
+			} else if (num % 3 == 0) {
+				msg = num + ": Fizz!";
+			} else if (num % 5 == 0) {
+				msg = num + ": Buzz!";
+			} else {
+				msg = num.ToString ();
+			}
+			return msg;
+		}
+
+		public static void Main (string[] args)
+		{
+			int[] nums = { 100, 22, 13, 15, 24, 32, 30, 40, 90, 55 };
+
+			foreach (int num in nums) {
+				Console.WriteLine (FizzBuzzMessage (num));
+			}
+		}
+	}
+}
