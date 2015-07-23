bec3fc59f9b91b99b01bd04470f03f24efaff409
Brian Tomlinson <darthlukan@gmail.com>
2015-07-23 16:59:59 +0200

    remove NuGet package nonsense, add basic GTKSharp template

---
+++ Variables/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Variables
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			/*
+			int x;
+			int y;
+
+			x = 7;
+			y = x + 3;
+			Console.WriteLine (y);
+			*/
+			Console.ReadLine ();
+		}
+	}
+}
