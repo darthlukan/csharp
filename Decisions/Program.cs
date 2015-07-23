442d54a15e203cdefdd116147c55e7c1ef7914ac
Brian Tomlinson <darthlukan@gmail.com>
2015-07-23 17:19:54 +0200

    Taking console input with ReadLine

---
+++ Decisions/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Decisions
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Enter a value:");
+			string value;
+			value = Console.ReadLine ();
+
+			Console.WriteLine ("You typed: " + value);
+			Console.ReadLine ();
+		}
+	}
+}
