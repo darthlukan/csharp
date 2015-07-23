8552d627141e2216c488283563f92805516438ce
Brian Tomlinson <darthlukan@gmail.com>
2015-07-23 17:30:36 +0200

    branching

--- Decisions/Program.cs
+++ Decisions/Program.cs
@@ -6,12 +6,29 @@ namespace Decisions
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Enter a value:");
-			string value;
-			value = Console.ReadLine ();
+//			Console.WriteLine ("Enter a value:");
+//			string value;
+//			value = Console.ReadLine ();
+//
+//			Console.WriteLine ("You typed: " + value);
+//			Console.ReadLine ();
 
-			Console.WriteLine ("You typed: " + value);
-			Console.ReadLine ();
+			string choice;
+			string message;
+			Console.WriteLine ("Door number 1, 2, or 3?");
+			choice = Console.ReadLine ();
+
+			if (choice == "1") {
+				message = "You won a car!";
+			} else if (choice == "2") {
+				message = "You won a boat!";
+			} else if (choice == "3") {
+				message = "You won a cat!";
+			} else {
+				message = "That was not one of the available choices. You win nothing!";
+			}
+
+			Console.WriteLine (message);
 		}
 	}
 }
